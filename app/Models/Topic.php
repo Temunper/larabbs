3826d29d19cdbebaec8b8af3ddbc4b9337fb7ab0
junpengtan <junpengtan@qq.com>
2022-09-15 12:05:56 +0800

    生成话题骨架

---
+++ app/Models/Topic.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Topic extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['title'];
+}
